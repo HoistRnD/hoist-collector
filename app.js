9ae14a07154c485b9ba2182c405260a952a23e2c
Owen Evans <owen@bgeek.net>
2014-10-26 21:27:03 +1300

    inital commit logs all steps to keen

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 'use strict';
 console.log('nothing here yet');
+var EventBroker = require('broker/lib/event_broker');
+var EventLogEvent = require('./lib/events/step_log_event');
+EventBroker.listen(EventLogEvent).then(function(){
+  console.log('listening');
+});
